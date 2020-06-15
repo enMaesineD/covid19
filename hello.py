cd12a664cffc8ac469a564237c25cfa731ab3802
enMaesineD <66967674+enMaesineD@users.noreply.github.com>
2020-06-15 14:54:58 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,20 @@
+#print("hello world")
+
+#name = input("best country in the world: ")
+#print(name)
+
+'''
+if (name.lower() == "canada"):
+    print(name + " is the best!")
+elif (name == "norway"):
+    print(name.lower() + " is the 2nd best!")
+else:
+    print("booo wrng answer")
+'''
+
+fruits = ['apple,grannymith, gala', 'orange,mandarin,bloodorange', 'banana,big,small', 'peach,sweet,notSweet']
+for fruit in fruits:
+    templist = fruit.split(',')
+    #print(fruit)
+    print(templist)
+    
