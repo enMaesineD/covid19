06b3e7bee508d897e2b13a6133c20570fe32e81d
enMaesineD <66967674+enMaesineD@users.noreply.github.com>
2020-06-17 13:29:42 -0400

    Add files via upload

---
+++ csv2json.py
@@ -0,0 +1,23 @@
+fileObj = open("06-15-2020.csv", "r") # r, w, or a are the options
+fileObj.readline()
+datalist = fileObj.readlines()
+fileObj.close()
+#print(datalist)
+
+conflist = []
+for country in datalist:
+    #print(country)
+    templist = country.split(",")
+    pname = templist[2]
+    cname = templist[3]
+    lat = templist[5]
+    lon = templist[6]
+    conf = int(templist[7])
+    #print(pname + cname + lat + lon + conf)
+    conflist.append({"pname":pname,"cname":cname,"lat":lat,"lon":lon,"conf":conf}
+)
+#print(conflist)
+conflist.sort(key=lambda s: s['conf'], reverse=True)
+fileout = open("06-15-2020.js","w")
+fileout.write("data = " + str(conflist))
+fileout.close()
